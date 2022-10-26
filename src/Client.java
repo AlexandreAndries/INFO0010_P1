506d985d0f1446fe4d3997b975f9e6a37ab0b196
AlexAndries <alexandreandries23@gmail.com>
2022-10-26 15:13:24 +0000

    set-up + src rep

---
+++ src/Client.java
@@ -0,0 +1,48 @@
+import java.lang.* ;
+import java.io.* ;
+import java.net.* ;
+import java.util.* ;
+
+// class Client - main program
+public class Client{
+    /*------------------------------------------------------------------------*/
+    /*- Variables ------------------------------------------------------------*/
+    /*------------------------------------------------------------------------*/
+    
+    /*------------------------------------------------------------------------*/
+    /*- Constructor ----------------------------------------------------------*/
+    /*------------------------------------------------------------------------*/
+
+    /*------------------------------------------------------------------------*/
+    /*- Methods --------------------------------------------------------------*/
+    /*------------------------------------------------------------------------*/
+    public byte[] query(byte[] bytesToSend) throws IOException {
+        // Initiate a new TCP connection with a Socket
+        Socket socket = new Socket( host: "Address of the server", port: 53);
+        OutputStream out = socket.getOutputStream() ;
+        InputStream in = socket.getInputStream() ;
+
+        // Send a query in the form of a byte array
+        out.write(bytesToSend) ;
+        out.flush();
+
+        // Retrieve the response length, as described in RFC 1035 (4.2.2 TCP usage)
+        byte[] lengthBuffer = new byte[2] ;
+        in.read(lengthBuffer); // Verify it returns 2
+
+        // Convert bytes to length (data sent over the network is always big-endian)
+        int length = ((lengthBuffer[0] & 0xff) << 8) | (lengthBuffer[1] & 0xff) ;
+
+        // Retrieve the full response
+        byte[] responseBuffer = new byte[length] ;
+        in.read(responseBuffer) ; // Verify it returns the value of "length"
+
+        return responseBuffer ;
+    }//fin query()
+    /*------------------------------------------------------------------------*/
+
+    /*------------------------------------------------------------------------*/
+    /*- Print ----------------------------------------------------------------*/
+    /*------------------------------------------------------------------------*/
+
+}//fin class Client
